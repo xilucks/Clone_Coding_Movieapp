31246ad6ef180a68e7dc1e6505198e562b9f1b8f
xilucks <siun0331@gmail.com>
2021-09-20 17:34:44 +0900

    props 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+
+function Food({name, picture}) {
+  return (
+    <div>
+      <h1> I like {name}</h1>
+      <img src = {picture}/>
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    name: 'kimchi',
+    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.koreatimes.co.kr%2Fwww%2Fculture%2F2021%2F06%2F141_309975.html&psig=AOvVaw0ELPm8gOPo967EGr1bzFRd&ust=1632211803156000&source=images&cd=vfe&ved=0CAkQjRxqFwoTCMjGqv2MjfMCFQAAAAAdAAAAABAD',
+
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1>Hello!</h1>
+    {foodILike.map(dish => (
+    <Food name = {dish.name} picture = {dish.image}/>))}
+  </div>
   );
 }
 
