a4c069a10b195a68ebc36210fb050f55088bab84
xilucks <siun0331@gmail.com>
2021-09-22 20:15:09 +0900

    state, 생명주기 함수 공부

--- src/App.js
+++ src/App.js
@@ -1,45 +1,48 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function Food({name, picture}) {
-  return (
-    <div>
-      <h1> I like {name}</h1>
-      <h4>{rating}/5.0</h4>
-      <img src = {picture} alt = {name}/>
-    </div>
-  );
-}
-
-const foodILike = [
-  {
-    id : 1,
-    name: 'kimchi',
-    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.koreatimes.co.kr%2Fwww%2Fculture%2F2021%2F06%2F141_309975.html&psig=AOvVaw0ELPm8gOPo967EGr1bzFRd&ust=1632211803156000&source=images&cd=vfe&ved=0CAkQjRxqFwoTCMjGqv2MjfMCFQAAAAAdAAAAABAD',
-    rating : 5,
-  },
-  {
-    id : 2,
-    name: 'Samgyeopsal',
-    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.koreatimes.co.kr%2Fwww%2Fculture%2F2021%2F06%2F141_309975.html&psig=AOvVaw0ELPm8gOPo967EGr1bzFRd&ust=1632211803156000&source=images&cd=vfe&ved=0CAkQjRxqFwoTCMjGqv2MjfMCFQAAAAAdAAAAABAD',
-    rating : 4.9,
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    console.log('hello');
   }
-];
+  state = {
+    count : 0,
 
-function App() {
-  return (
-  <div>
-    <h1>Hello!</h1>
-    {foodILike.map(dish => (
-    <Food key = {dish.id} name = {dish.name} picture = {dish.image} rating = {dish.rating}/>))}
-  </div>
-  );
+  };
+
+  add = () => {
+    //this.setState({count : this.count.state +1});
+    this.setState(current => ({
+      count: current.count + 1,
+    }));
+  };
+
+  minus = () => {
+    //this.setState({count : this.count.state -1});
+    this.setState(current => ({
+      count: current.count - 1,
+    }));
+  };
+
+  componentDidMount(){
+    console.log('component rendered');
+  }
+
+  componentDidUpdate(){
+    console.log('updated');
+  }
+
+  render() {
+    console.log('render');
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onclick = {this.add}>Add</button>
+        <button onclick = {this.minus}>Minus</button>
+      </div>
+    )
+  }
+
 }
-
-Food.PropTypes = {
-  name: PropTypes.string.isRequired,
-  picture : PropTypes.string.isRequired,
-  rating : PropTypes.number.isRequired,
-};
 
 export default App;
