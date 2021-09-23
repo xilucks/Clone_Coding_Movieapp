20e519ab50e4ed8eb61ce4b67f0a124e128607e4
xilucks <siun0331@gmail.com>
2021-09-24 00:11:27 +0900

    로딩 화면 구현

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: [],
   };
+  getMovies = async() => {
+    const {
+      data : {
+        data : { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({ movies, isLoading: false});
+  };
 
   componentDidMount(){
-    setTimeout( () => {
-      this.setState({ isLoading: false});
-    }, 6000);
+    this.getMovies();
   }
 
   render() {
     const {isLoading} = this.state; 
-    return <div>{isLoading? 'Loading...' : 'We are ready'}</div>
+    return <div> {isLoading ? 'Loading...' : 'We are ready'}</div>
   }
-
+ 
 }
 
 export default App;
