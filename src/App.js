4922cb22bb2d7f48c40491218918d012e7bed6a0
xilucks <siun0331@gmail.com>
2021-09-24 17:22:40 +0900

    라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,53 +1,16 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
 import './App.css';
+import {HashRouter, Route} from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home'
 
-
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-  getMovies = async() => {
-    const {
-      data : {
-        data : {movies},
-      },
-    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
-    this.setState({ movies, isLoading: false});
-  };
-
-  componentDidMount(){
-    this.getMovies();
-  }
-
-  render() {
-    const {isLoading, movies} = this.state; 
-    return (
-      <section className = "container">
-        {isLoading? (
-            <div className = "loader">
-            <span className = "loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className = "moveis">
-            {movies.map (movie => (
-              <Movie 
-                key = {movie.id}
-                id = {movie.id}
-                year = {movie.year}
-                title = {movie.title}
-                summary = {movie.summary}
-                poster = {movie.poster}
-                genres = {movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Route path= "/" component = {home} />
+      <Route path="/about" component ={About} />
+    </HashRouter>
+  )
 }
 
 export default App;
